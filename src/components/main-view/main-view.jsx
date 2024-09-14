4d195dd5eda205f5700f028a6ed1137416094ebf
jsy5888 <oliviasunjin0318@gmail.com>
2024-09-14 15:01:58 -0400

    exercise 3.5

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,15 +1,33 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
-
+import { LoginView } from "../login-view/login-view";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    fetch("https://openlibrary.org/search.json?q=star+wars");
-  },[]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] =useState(null);
+
+useEffect(() => {
+  fetch("https://openlibrary.org/search.json?q=star+wars")
+  .then((Response) => Response.json())
+  .then((data)=> {
+    const moviesFromApi = data.docs.map((doc) => {
+      return {
+        id : doc.key,
+        title: doc.title,
+        image: `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
+        author: doc.author_name?.[0]
+      };
+    });
+
+   setMovies(moviesFromApi);
+    });
+}, []);
+
+if (!user) {
+  return<LoginView/>;
+}
 
   if (selectedMovie) {
     return (
@@ -35,7 +53,9 @@ export const MainView = () => {
       ))}
     </div>
   );
-}
+};
+
+
 // export const MainView = () => {
 //     return (
 //       <div>
