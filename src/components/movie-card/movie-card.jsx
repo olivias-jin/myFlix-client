73b2a86e405e11aab9946e6345ff1875b27db20c
jsy5888 <oliviasunjin0318@gmail.com>
2024-10-08 19:23:09 -0400

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,4 @@
-// Here you import the PropTypes library
+// import the PropTypes library
 import PropTypes from "prop-types";
 
 // The Movie Card function component
@@ -14,10 +14,22 @@ export const MovieCard = ({ movie, onMoiveClick }) => {
     );
 };
 
-//  Here is where we define all the props constraints for the MovieCard
+//  where define all the props constraints for the MovieCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
+};
+
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }).isRequired,
+    ImagePath: PropTypes.string
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired
 };
