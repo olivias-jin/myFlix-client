b329a71a150255bd7632a198f7cfad9f6384e413
jsy5888 <oliviasunjin0318@gmail.com>
2024-10-10 21:00:39 -0400

    updated

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -9,6 +9,7 @@ export const SignupView = () => {
 
     const handleSubmit = (event) => { 
         event.preventDefault();
+       
         const data = {
           Username: username,
           Password: password,
