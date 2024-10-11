b2b5d23f440557ba4f53b1eb64307aaefacd5f31
jsy5888 <oliviasunjin0318@gmail.com>
2024-10-10 22:40:07 -0400

    <Container> Bootstrap Component

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,17 @@ import { createRoot } from "react-dom/client";
 // Import statement to indicate that you need to bundle `./index.scss`
 import { MainView } from "./components/main-view/main-view";
 
+import "bootstrap/dist/bootstrap.min.css";
+
 import "./index.scss";
+import Container from "react-bootstrap/Container";
 
 // Main component (will eventually use all the others)
 const App = () => {
-    return <MainView />;
+    return 
+    <Container> style={{border: "1px solid red"}}
+        <MainView />
+    </Container>;
    };
 
 // Finds the root of your app
