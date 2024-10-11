2f992ff8ea925e1e5a8412cf82cd6073a1eb69cb
jsy5888 <oliviasunjin0318@gmail.com>
2024-10-10 22:06:46 -0400

    login button updated

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,9 @@
 import React, {useState} from "react";
+import { FormGroup } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+
 
 export const LoginView = ({onLoggedIn}) => {
 
@@ -34,26 +39,31 @@ export const LoginView = ({onLoggedIn}) => {
 
 
     return (
-        <form onSubmit={handleSubmit}> 
-            <label>
-                Username:
-                <input 
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-                />
-            </label>
-            <label>
-                Password:
-                <input 
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                 />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
+        <Form onSubmit={handleSubmit}> 
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            minLength="3"
+            />
+        </Form.Group>
+
+        <FormGroup controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            />
+          </FormGroup>
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+        </Form>
     );
 };
