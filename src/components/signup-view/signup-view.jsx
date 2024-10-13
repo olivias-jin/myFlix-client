1847d77318db2bd82cd793b9874bb12f8e3dd560
jsy5888 <oliviasunjin0318@gmail.com>
2024-10-13 00:50:08 -0400

    updated

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,78 +1,97 @@
-import{ useState } from "react";
+import { useState } from "react";
+import { FormLabel } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 
 export const SignupView = () => {
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
 
-    const [username, setUsername] =useState("");
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
-    const [birthday, setBirthday] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    const handleSubmit = (event) => { 
-        event.preventDefault();
-       
-        const data = {
-          Username: username,
-          Password: password,
-          Email: email,
-          Birthday: birthday,
-        };
-    
-        fetch("https://morning-taiga-69315-198698fb21c5.herokuapp.com/users", {
-            method: "POST",
-          body: JSON.stringify(data),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }).then((response) => {
-          if (response.ok) {
-            alert("Signup successful");
-            window.location.reload();
-          } else {
-            alert("Signup failed");
-          }
-        });
-      };
+    const data = {
+      Name: name,
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday,
+    };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username:
-                <input
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-                minLength="3"
-                />
-            </label>
-            <label>
-                Password:
-                <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                />
-            </label>
-            <label>
-                Email:
-                <input 
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                />
-            </label>
-            <label>
-                Birthday:
-                <input
-                type="date"
-                value={birthday}
-                onChange={(e) => setBirthday(e.target.value)}
-                required
-                />            
-            </label>
-            <button type="submit">Submit</button>
-        </form>
-    );
+    fetch("https://morning-taiga-69315-198698fb21c5.herokuapp.com/users", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => {
+      if (response.ok) {
+        alert("Signup successful");
+        window.location.reload();
+      } else {
+        alert("Signup failed");
+      }
+    });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formName">
+        <Form.Label>Name:</Form.Label>
+        <Form.Control
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="3"
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formEmail">
+        <Form.Label>Email:</Form.Label>
+        <Form.Control
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formBirthday">
+        <Form.Label>Birthday:</Form.Label>
+        <Form.Control
+          type="date"
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+          required
+        />
+        </Form.Group>
+
+      <Button variant="primary" className="mt-3" type="submit">Submit</Button>
+    </Form>
+  );
 };
