8b58e00e19edc6c63d88ecdcadfc408ad6ff42f1
jsy5888 <oliviasunjin0318@gmail.com>
2024-10-13 22:10:38 -0400

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,29 +3,31 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { prototype } from "jsonwebtoken/lib/JsonWebTokenError";
 
 // The Movie Card function component
-export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <Card className="h-100">
-        <Card.Img variant="top" src={movie.image}/>
-        <Card.Body>
-          <Card.Title>{movie.title}</Card.Title>
-          <Card.Text>{movie.author}</Card.Text>
-          <Button onClick={() => onMovieClick(movie)} variant="link">
-            Open
-          </Button>
-        </Card.Body>
-      </Card>
-    );
+export const MovieCard = ({ movie }) => {
+  return (
+    <Card>
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.author}</Card.Text>
+        <Link to={`/movies/$encodeURIComponent(movie.id)`}>
+          <Button variant="link">Open</Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
 };
 
 //  where define all the props constraints for the MovieCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+    Title: PropTypes.string.isRequired,
+    Image: PropTypes.string.isRequired,
+    Author: prototype.string
+  }).isRequired
 };
 
 
