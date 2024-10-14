fd87163c68711d600f905f5d199d95947e668185
jsy5888 <oliviasunjin0318@gmail.com>
2024-10-13 23:54:56 -0400

    Update profile-view.jsx

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Button, Card } from "react-bootstrap";
+import './profile-view.scss';
+
+
+export const ProfileView ({ user, movies, onUpdatedUserInfo, onDeleteUser }) => {
+
+    // Updated User Info
+    const [user, setUser] = useState(localUser.Username || "");
+    const [password, setPassword] =useState(localUser.password || "");
+    const [email, setEmail] =useState(localUser.email || "");
+    const [birthday, setBirthday] =useState(localUser.birthday || "01/01/0001");
+
+
+    // Deregister
+    const handleDelete = () => {
+        if (window.confirm("Are you sure you want to delete your account? This action cannot be undone.")
+        {
+            onDeleteUser(user.id);
+        })
+    };
+
+    return (
+        <Row className="justify-content-md-center">
+            <Card>
+                <Col>
+                    <Card.Header>
+                        <UserInfo
+                            name={user.Username}
+                            email={user.Email}
+                        />
+                    </Card.Header>
+                </Col>
+
+                <Col>
+                    <Card.Body>
+                        <ProfileUpdate
+                            username={username}
+                            password={password}
+                            email={email}
+                            birthday={birthday}
+                        />
+                    </Card.Body>
+                    <Card.Body>
+                        <Button variant="danger" onClick={handleDelete}>
+                            Delete User
+                        </Button>
+                    </Card.Body>
+                </Col>
+            </Card>
+        </Row >
+    )
+
+
+}
+
+
+
+
+
+
