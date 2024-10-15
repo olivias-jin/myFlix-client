ba11d94387b72b55dd33f2f803d0676fe85f5e24
jsy5888 <oliviasunjin0318@gmail.com>
2024-10-14 21:44:47 -0400

    Update navigation-bar.jsx

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -6,7 +6,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand as={Link} to="/">
-          Books App
+          Movies App
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -21,7 +21,13 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                 <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
                 <Nav.Link as={Link} to="/login">Login</Nav.Link>
               </>
-            )}
+            ) : (
+              <>
+              <Nav.Link as={Link} to="/Profile">Profile</Nav.Link>
+              </>
+            )
+            
+            }
           </Nav>
         </Navbar.Collapse>
       </Container>
