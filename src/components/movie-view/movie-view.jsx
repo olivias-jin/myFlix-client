e489a493c07236a427ef8f6ac531d8019838eaaa
jsy5888 <oliviasunjin0318@gmail.com>
2024-10-16 00:21:41 -0400

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,11 +1,68 @@
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
+import { Button, Card, Row } from "react-bootstrap";
 import "./movie-view.scss";
 
-export const MovieView = ({ movies }) => {
+export const MovieView = ({ movies, user, token, setUser }) => {
   const { movieId } = useParams();
-
+  const [isFavorite, setIsFavorite] = useState(false);
   const movie = movies.find((b) => b.id === movieId);
+
+
+  useEffect(() => {
+    if (user && user.FavoriteMovies) {
+      const isFavorite = user.FavoriteMovies.includes(movieId);
+      setIsFavorite(isFavorite);
+    }
+  }, [movieId, user]);
+
+
+  const addtoFavorite = () => {
+    fetch(`https://morning-taiga-69315-198698fb21c5.herokuapp.com/users/${user.Username}/${movieId}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        }
+      }).then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        setUser(data);
+        localStorage.setItem("user", JSON.stringify(data));
+        setIsFavorite(true);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const removefromFavorite = () => {
+    fetch(`https://morning-taiga-69315-198698fb21c5.herokuapp.com/users/${user.Username}/${movieId}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        }
+      }).then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        setUser(data);
+        localStorage.setItem("user", JSON.stringify(data));
+        setIsFavorite(false);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
   return (
     <div>
