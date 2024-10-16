88a73a115b49117e60b695b9b07bec7c60233d38
jsy5888 <oliviasunjin0318@gmail.com>
2024-10-15 23:33:26 -0400

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,15 +5,16 @@ import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 // The Movie Card function component
-export const MovieCard = ({ movie }) => {
+export const MovieCard = ({ movie, handleAddToFavorite }) => {
   return (
     <Card>
       <Card.Img variant="top" src={movie.image} />
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
         <Card.Text>{movie.author}</Card.Text>
-        <Link to={`/movies/$encodeURIComponent(movie.id)`}>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
           <Button variant="link">Open</Button>
+          <Button onClick={() => handleAddToFavorite(movie)} variant="link">Add  Favorite</Button>
         </Link>
       </Card.Body>
     </Card>
