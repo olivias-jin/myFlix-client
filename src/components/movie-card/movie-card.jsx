1f94aec8e4c7554d880cde3a6402bd312beb2715
jsy5888 <oliviasunjin0318@gmail.com>
2024-10-16 20:11:26 -0400

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,7 +5,7 @@ import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 // The Movie Card function component
-export const MovieCard = ({ movie, handleAddToFavorite }) => {
+export const MovieCard = ({ movie, handleAddToFavorite  }) => {
   return (
     <Card>
       <Card.Img variant="top" src={movie.image} />
@@ -14,7 +14,7 @@ export const MovieCard = ({ movie, handleAddToFavorite }) => {
         <Card.Text>{movie.author}</Card.Text>
         <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
           <Button variant="link">Open</Button>
-          <Button onClick={() => handleAddToFavorite(movie)} variant="link">Add  Favorite</Button>
+          <Button onClick={() => handleAddToFavorite(movie)} variant="link">Add Favorite</Button>
         </Link>
       </Card.Body>
     </Card>
@@ -24,9 +24,9 @@ export const MovieCard = ({ movie, handleAddToFavorite }) => {
 //  where define all the props constraints for the MovieCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
-    Image: PropTypes.string.isRequired,
-    Author: PropTypes.string
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    author: PropTypes.string
   }).isRequired
 };
 
