766ef01233d0741090662d7f391438c0fe049867
jsy5888 <oliviasunjin0318@gmail.com>
2024-10-17 00:09:01 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 import Container from "react-bootstrap/Container";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
 
